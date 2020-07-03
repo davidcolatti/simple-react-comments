ffba2f8ca5bafe96893e59095a4af184b395cd65
David Colatti <davidcolatti@gmail.com>
2020-07-03 14:59:01 -0400

    feat: comment box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import CommentsBlock from "simple-react-comments";
 
-function App() {
-  return <div className="App">App</div>;
-}
+const commentsData = [
+  {
+    fullName: "David",
+    createdAt: new Date(),
+    text: "Hi, it's David",
+  },
+  {
+    fullName: "Luis",
+    createdAt: new Date(),
+    text: "Hi, it's Luis",
+  },
+  {
+    fullName: "Garrett",
+    createdAt: new Date(),
+    text: "Hi, it's Garrett",
+  },
+];
+
+const App = () => {
+  const [comments, setComments] = useState(commentsData);
+
+  return (
+    <div>
+      <CommentsBlock
+        comments={comments}
+        signinUrl={"/signin"}
+        isLoggedIn
+        reactRouter={false} // set to true if you are using react-router
+        onSubmit={(text) => {
+          if (text.length > 0) {
+            setComments([
+              ...comments,
+              {
+                fullName: "Name",
+                createdAt: new Date(),
+                text,
+              },
+            ]);
+            console.log("submit:", text);
+          }
+        }}
+      />
+    </div>
+  );
+};
 
 export default App;
